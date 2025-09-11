e1f0f9ffc44ab707908e91bf5c37424eef44683d
rakeshshit <iamrakeshit60@gmail.com>
2025-09-11 23:01:52 +0530

    273 : jwt token is created

--- src/main/java/com/authService/controller/AuthController.java
+++ src/main/java/com/authService/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.authService.payload.APIResponse;
 import com.authService.payload.LoginDto;
 import com.authService.payload.UserDto;
 import com.authService.repository.UserRepository;
+import com.authService.service.JWTService;
 import com.authService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/auth")
 public class AuthController {
 
+    @Autowired
+    private JWTService jwtService;
     @Autowired
     private AuthenticationManager authManager;
     @Autowired
@@ -42,9 +45,14 @@ public class AuthController {
         try{
             Authentication authenticate = authManager.authenticate(token);
             if(authenticate.isAuthenticated()){
+
+                String jwtToken = jwtService.generateToken(dto.getUsername(),
+                        authenticate.getAuthorities().iterator().next().getAuthority());
+
+
                 response.setMessage("User Logged In Successfully!");
                 response.setStatus(200);
-                response.setData("Success");
+                response.setData(jwtToken);
                 return new ResponseEntity<>(response, HttpStatusCode.valueOf(response.getStatus()));
             }
         }catch (Exception e){
--- src/main/java/com/authService/service/JWTService.java
+++ src/main/java/com/authService/service/JWTService.java
@@ -1,4 +1,25 @@
 package com.authService.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class JWTService {
+
+    private static final String SECRET_KEY = "my-super-secret-key";
+    private static final long EXPIRATION_TIME = 86400000;
+
+    public String generateToken(String username, String role){
+
+        // CSCIES
+        return JWT.create()
+                .withSubject(username)
+                .withClaim("role", role)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(Algorithm.HMAC256(SECRET_KEY));
+    }
 }
